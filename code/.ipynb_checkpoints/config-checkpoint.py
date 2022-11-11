dd600f0e6990c9b686792e1026a1a6568bf26fa4
dcavaliere <dylanjamescavaliere@gmail.com>
2022-11-10 17:48:00 -0800

    updated recommendation engine

--- code/.ipynb_checkpoints/config-checkpoint.py
+++ code/.ipynb_checkpoints/config-checkpoint.py
@@ -1,3 +1,3 @@
 #throw my keys into a .py file and call to it here so that my keys are hidden
-cid = "261056e144784273803eeb3c020ad72c"
-secret = "aa18ea69cacb4ba2826c3664cfd8db14"
+cid = "768f1609a72343fd8242273055d3c1e9"
+secret = "1ac9ee68636648c593615bb198865af2"
